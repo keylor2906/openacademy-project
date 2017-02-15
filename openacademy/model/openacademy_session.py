69a10bc1ed2f071c70a6f6827545bd9b6c2512e5
Jose Robles <josemanuel@vauxoo.com>
2017-02-15 15:56:20 +0000

    [IMP] openacademy: Add workflow, transition and activities

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -36,7 +36,7 @@ class Session(models.Model):
                               ('draft', "Draft"),
                               ('confirmed', "Confirmed"),
                               ('done', "Done"),
-                             ], default='draft')
+                             ])
 
     @api.multi
     def action_draft(self):
